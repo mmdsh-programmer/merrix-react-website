ad58e4129084e5af29ae2cd8da9c57111e248ee7
mmdsh-programmer <programmer.mmdsh@gmail.com>
2021-09-04 22:36:11 +0430

    change api url

--- src/services/crud/coupons.js
+++ src/services/crud/coupons.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-const baseURL = "https://merrix.com/wp-json";
+const baseURL = "https://admin.merrix.com/wp-json";
 let config = {
     method: "get",
-    url: "https://merrix.com/wp-json/wc/v3/coupons",
+    url: "https://admin.merrix.com/wp-json/wc/v3/coupons",
     headers: {
         Authorization:
             "Basic Y2tfY2JhYmE5Mzk2YTliNWUxMTE2OTcwZmRjOTgxMjc2YWZlNWQ0MjQ1NDpjc183NTcxZDYzMjFlNTMxZTM4NWQwOWZkOTg4ZTUzMDkzNDlhNWE4OTNk",
--- src/services/crud/order.js
+++ src/services/crud/order.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-const baseURL = "https://merrix.com/wp-json";
+const baseURL = "https://admin.merrix.com/wp-json";
 let config = {
     method: "post",
-    url: "https://merrix.com/wp-json/wc/v3/orders",
+    url: "https://admin.merrix.com/wp-json/wc/v3/orders",
     headers: {
         Authorization:
             "Basic Y2tfY2JhYmE5Mzk2YTliNWUxMTE2OTcwZmRjOTgxMjc2YWZlNWQ0MjQ1NDpjc183NTcxZDYzMjFlNTMxZTM4NWQwOWZkOTg4ZTUzMDkzNDlhNWE4OTNk",
