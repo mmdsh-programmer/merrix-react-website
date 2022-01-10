dface9e8b583074a1d95b606bfec4d7cb8b88b56
mmdsh-programmer <programmer.mmdsh@gmail.com>
2022-01-10 23:58:28 +0330

    increase material selection height

--- src/components/FilterComponent.jsx
+++ src/components/FilterComponent.jsx
@@ -176,7 +176,7 @@ const ITEM_PADDING_TOP = 8;
 const MenuProps = {
   PaperProps: {
     style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      maxHeight: ITEM_HEIGHT * 7.5 + ITEM_PADDING_TOP,
       width: 250,
     },
   },
